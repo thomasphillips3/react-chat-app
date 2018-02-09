ec14209034f1743632d0f6210e4b052b1a82ee96
Thomas Phillips <thomasphillips3@gmail.com>
2018-02-09 00:57:01 -0800

    Add current user to the user list

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,19 @@
 import * as types from '../constants/ActionTypes'
 
 let nextMessageId = 0
-const nextUserId = 0
+let nextUserId = 0
 
 export const addMessage = (message, author) => ({
   type: types.ADD_MESSAGE,
   id: nextMessageId++,
   message,
   author
+})
+
+export const addUser = name => ({
+  type: types.ADD_USER,
+  id: nextUserId++,
+  name
 })
 
 export const messageReceived = (message, author) => ({
--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
-
+import { addUser } from './actions'
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import chat from './reducers'
 
 const store = createStore(chat)
+
+// Register current user as present in the chat
+store.dispatch(addUser('Me'))
 
 ReactDOM.render(
   <Provider store={store}>
