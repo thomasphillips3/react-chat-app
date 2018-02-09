89235594a86956e2a602b06f12edf20d70fc7017
Thomas Phillips <thomasphillips3@gmail.com>
2018-02-09 00:38:49 -0800

    Add Redux to manage the state
    Creates the basic UI, and uses Redux for state management.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import chat from './reducers'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const store = createStore(chat)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
+registerServiceWorker()
